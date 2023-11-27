2e3667b557a38c7f5d954126efe37075c463a5f6
effiecancode <faitheffie25@gmail.com>
2023-11-27 15:09:51 +0300

    Hositing

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
-export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+let task = false;
+let task2 = true;
 
+export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     task = true;
     task2 = false;
