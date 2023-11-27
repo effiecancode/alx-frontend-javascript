c2fc0cf1a1c4f3b4d290079f88fc1b84a108f848
effiecancode <faitheffie25@gmail.com>
2023-11-27 12:37:29 +0300

    const/var

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,18 @@
+// function taskFirst to instantiate variables using const
+// function taskNext to instantiate variables using let
+
+export function taskFirst() {
+    const task = 5;
+    return task;
+  }
+
+  export function getLast() {
+    return 10;
+  }
+
+  export function taskNext() {
+    let combination = taskFirst();
+    combination += getLast();
+
+    return combination;
+  }
