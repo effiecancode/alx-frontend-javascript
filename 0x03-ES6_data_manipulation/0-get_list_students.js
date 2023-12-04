44748dcfcf51d683dad51adf5fe2f892d827b535
effiecancode <faitheffie25@gmail.com>
2023-12-04 15:15:36 +0300

    Create an array of objects 🤩

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,29 @@
+export default function getListStudents() {
+  const obj1 = {
+    name: 'Guillaume',
+    id: 1,
+    location: 'San Francisco',
+  };
+
+  const obj2 = {
+    name: 'James',
+    id: 2,
+    location: 'Columbia',
+  };
+
+  const obj3 = {
+    name: 'Serena',
+    id: 5,
+    location: 'San Fancisco',
+  };
+
+  // had initially used (...) spread oparator, but it unpacks and merges
+  // i need to return an array of objects, not merged elements
+  const arr = [obj1, obj2, obj3];
+  return arr;
+}
+
+// function test
+
+// const list = getListStudents();
+// console.log(list);
