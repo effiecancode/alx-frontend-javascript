4c3d187b556308bcdff55a437427fb26e4a5c992
effiecancode <faitheffie25@gmail.com>
2023-12-04 17:15:56 +0300

    use reduce array method

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,23 @@
+import getListStudentIds from './1-get_list_student_ids';
+import getListStudents from './0-get_list_students';
+
+export default function getStudentIdsSum(getListStudents) {
+  // get the list of students
+  const obj = getListStudents();
+
+  // extract ids
+  const ids = getListStudentIds(obj);
+
+  // accumulate the ids
+  sum = ids.reduce((accumulator, currentId) => accumulator + currentId, 0);
+
+  return sum;
+}
+
+// test the function
+
+const obj = getListStudents();
+const ids = getListStudentIds(obj);
+const idsum = getStudentIdsSum(ids);
+
+console.log(idsum);
