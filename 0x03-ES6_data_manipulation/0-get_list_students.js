66f58840e6459c50fd5dd83e7739c090b2649226
effiecancode <faitheffie25@gmail.com>
2023-12-05 09:59:41 +0300

    fix indentation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,22 @@
 export default function getListStudents() {
-  return [{
-    name: 'Guillaume',
-    id: 1,
-    location: 'San Francisco',
-  },
+  return [
+    {
+      name: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
+    },
 
-  {
-    name: 'James',
-    id: 2,
-    location: 'Columbia',
-  },
+    {
+      name: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
 
-  {
-    name: 'Serena',
-    id: 5,
-    location: 'San Francisco',
-  },
+    {
+      name: 'Serena',
+      id: 5,
+      location: 'San Francisco',
+    },
   ];
 }
 
