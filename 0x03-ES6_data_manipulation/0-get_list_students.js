838a4cb16dfaba2bfb214929d908690575cdb494
effiecancode <faitheffie25@gmail.com>
2023-12-05 09:34:58 +0300

    change syntax

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,26 +1,23 @@
 export default function getListStudents() {
-  const obj1 = {
+  const obj1 = [{
     name: 'Guillaume',
     id: 1,
     location: 'San Francisco',
-  };
+  },
 
-  const obj2 = {
+  {
     name: 'James',
     id: 2,
     location: 'Columbia',
-  };
+  },
 
-  const obj3 = {
+  {
     name: 'Serena',
     id: 5,
     location: 'San Francisco',
-  };
-
-  // had initially used (...) spread oparator, but it unpacks and merges
-  // i need to return an array of objects, not merged elements
-  const arr = [obj1, obj2, obj3];
-  return arr;
+  },
+  ];
+  return obj1;
 }
 
 // function test
